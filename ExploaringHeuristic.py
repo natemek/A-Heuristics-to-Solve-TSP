70137ce00106f202b70676128d2953e2c32ce69a
natemek <natnaelmek@gmail.com>
2020-10-28 13:13:16 -0400

    added remaining heuristics

--- ExploaringHeuristic.py
+++ ExploaringHeuristic.py
@@ -2,15 +2,17 @@ from util import *
 import heapq
 import sys
 import time
+import random
+import numpy as np
 
 def main(argv):
     if argv:
         filename = argv[1]
         func = argv[0]
-    else:  # default filename
-        filename = 'infile05_01.txt'
+    else:  # default filename and func
+        filename = 'input_s5.txt'
         func = 'ucs'
-        
+
     matrix = get_matrix(filename)
     if matrix:
         start = 0
@@ -19,6 +21,12 @@ def main(argv):
         initial_cpu = time.process_time()
         if func == 'ucs':
             print(A_uniformCost(matrix, start), time.time() - initial_time, time.process_time() - initial_cpu)
+        elif func == 'rre':
+            print(A_randomEdge(matrix, start), time.time() - initial_time, time.process_time() - initial_cpu)
+        elif func == 'cre':
+            print(A_cheapestEdge(matrix, start), time.time() - initial_time, time.process_time() - initial_cpu)
+        elif func == 'mst':
+            print(A_MST(matrix, start), time.time() - initial_time, time.process_time() - initial_cpu)
         
 
 def A_uniformCost(matrix, start):
@@ -50,12 +58,7 @@ def A_uniformCost(matrix, start):
         curr_node = path[-1]
         for neighbour_node, neighbour_cost in enumerate(matrix[curr_node]):
             # if visited all nodes and attempting to go back to the initial
-
             if len(path) == len(matrix):
-                # return_cost, return_path, nodes_expanded = shortest_path_uniformCost(matrix, neighbour_node, start)
-                # final_path = path + return_path
-                # total_cost = prev_cost + neighbour_cost + return_cost
-                # num_of_nodes_expanded += nodes_expanded
                 total_cost = prev_cost + matrix[curr_node][start]
                 final_path = path + [start]
                 heapq.heappush(queue, (total_cost, final_path))
@@ -71,6 +74,164 @@ def A_uniformCost(matrix, start):
 
         # print(queue, "\n\n")
 
+def A_randomEdge(matrix, start):
+    """ Implements A* search with random remaining edge heuristic on an adjesency matrix to solve TSP
+
+      :param matrix: adjesency matrix
+      :param start:  start node
+
+      :return: returns (cost to goal, path, num of nodes visited)
+    """
+    num_of_nodes_expanded = 1
+    queue = []  # Frontier
+
+    """ The queue structure holds (heuristic, path, path_cost) where
+
+    :heuristic: path_cost + weight of random remaining edge
+    :path: path to current from start
+    :path_cost: cost of path from start to current node
+  """
+    heapq.heappush(queue, (0 + random.choice(remainingEdges(matrix, [start])), [start], 0))
+
+    while queue:
+        prev_cost, path, prev_total_cost = heapq.heappop(queue)
+
+        # goal state
+        if len(path) >= len(matrix) + 1 and path[0] == path[-1]:
+            return (prev_total_cost, path, num_of_nodes_expanded)
+
+        # expand curr node
+        curr_node = path[-1]
+        for neighbour_node, neighbour_cost in enumerate(matrix[curr_node]):
+            # if visited all nodes and attempting to go back to the initial
+
+            if len(path) >= len(matrix):
+                return_cost = matrix[curr_node][start]
+                total_cost = prev_total_cost + return_cost
+                final_path = path + [start]
+                heapq.heappush(queue, (total_cost + random.choice(remainingEdges(matrix, final_path)), final_path, total_cost))
+                num_of_nodes_expanded += 1
+                break
+
+            elif neighbour_node not in path:
+                path_to_neighbour = path[:]
+                path_to_neighbour.append(neighbour_node)
+                total_cost = neighbour_cost + prev_total_cost
+                heapq.heappush(queue, (total_cost + random.choice(remainingEdges(matrix, path_to_neighbour)), path_to_neighbour, total_cost))
+                num_of_nodes_expanded += 1  
+
+        # print(queue, "\n\n")
+
+def A_cheapestEdge(matrix, start):
+    """ Implements A* search with cheapest remaining edge heuristic on an adjesency matrix to solve TSP
+
+      :param matrix: adjesency matrix
+      :param start:  start node
+
+      :return: returns (cost to goal, path, num of nodes visited)
+    """
+    num_of_nodes_expanded = 1
+    queue = []  # Frontier
+
+    """ The queue structure holds (heuristic, path, path_cost) where
+
+    :heuristic: path_cost + weight of cheapest remaining edge
+    :path: path to current from start
+    :path_cost: cost of path from start to current node
+  """
+    heapq.heappush(queue, (min(remainingEdges(matrix, [start])), [start], 0))
+
+    while queue:
+        prev_cost, path, prev_total_cost = heapq.heappop(queue)
+
+        # goal state
+        if len(path) >= len(matrix) + 1 and path[0] == path[-1]:
+            return (prev_total_cost, path, num_of_nodes_expanded)
+
+        # expand curr node
+        curr_node = path[-1]
+        for neighbour_node, neighbour_cost in enumerate(matrix[curr_node]):
+            # if visited all nodes and attempting to go back to the initial
+            if len(path) >= len(matrix):
+                return_cost = matrix[curr_node][start]
+                total_cost = prev_total_cost + return_cost
+                final_path = path + [start]
+                heapq.heappush(queue, (total_cost + min(remainingEdges(matrix, final_path)), final_path, total_cost))
+                num_of_nodes_expanded += 1
+                break
+
+            elif neighbour_node not in path:
+                path_to_neighbour = path[:]
+                path_to_neighbour.append(neighbour_node)
+                total_cost = neighbour_cost + prev_total_cost
+                heapq.heappush(queue, (total_cost + min(remainingEdges(matrix, path_to_neighbour)), path_to_neighbour, total_cost))
+                num_of_nodes_expanded += 1  
+
+        # print(queue, "\n\n")
+
+def A_MST(matrix, start):
+    """ Implements A* search with minimum spanning tree heuristic on an adjesency matrix to solve TSP
+
+      :param matrix: adjesency matrix
+      :param start:  start node
+
+      :return: returns (cost to goal, path, num of nodes visited)
+    """
+    num_of_nodes_expanded = 1
+    queue = []  # Frontier
+
+    """ The queue structure holds (heuristic, path, path_cost) where
+
+    :heuristic: path_cost + weight of MST on remaining edge
+    :path: path to current from start
+    :path_cost: cost of path from start to current node
+  """
+    heapq.heappush(queue, (0, [start], 0))
+
+    while queue:
+        prev_cost, path, prev_total_cost = heapq.heappop(queue)
+
+        # goal state
+        if len(path) >= len(matrix) + 1 and path[0] == path[-1]:
+            return (prev_total_cost, path, num_of_nodes_expanded)
+
+        # expand curr node
+        curr_node = path[-1]
+        for neighbour_node, neighbour_cost in enumerate(matrix[curr_node]):
+            # if visited all nodes and attempting to go back to the initial
+            if len(path) >= len(matrix):
+                return_cost = matrix[curr_node][start]
+                total_cost = prev_total_cost + return_cost
+                final_path = path + [start]
+                m_ = np.array(matrix)
+                idx_to_keep = np.array(remainingNodes(len(matrix), final_path))
+                
+                if len(idx_to_keep) == 0:
+                    heuristic = 0
+                else:
+                    m_ = m_[idx_to_keep[:,None], idx_to_keep]
+                    heuristic = kruskalMST(m_)
+
+                heapq.heappush(queue, (total_cost + heuristic, final_path, total_cost))
+                num_of_nodes_expanded += 1
+                break
+
+            elif neighbour_node not in path:
+                path_to_neighbour = path[:]
+                path_to_neighbour.append(neighbour_node)
+                total_cost = neighbour_cost + prev_total_cost
+                # create an np appay of the adj matrix to run mst on modified adj matrix
+                m_ = np.array(matrix)
+                idx_to_keep = np.array(remainingNodes(len(matrix), path_to_neighbour) + [neighbour_node])
+                if len(idx_to_keep) == 0:
+                    heuristic = 0
+                else:
+                    m_ = m_[idx_to_keep[:,None], idx_to_keep]
+                    heuristic = kruskalMST(m_)
+                heapq.heappush(queue, (total_cost + heuristic, path_to_neighbour, total_cost))
+                num_of_nodes_expanded += 1  
+
+        # print(queue, "\n\n")
 
 # Entry point
 if __name__ == '__main__':
--- util.py
+++ util.py
@@ -19,3 +19,66 @@ def get_matrix(filename):
         matrix.append(temp)
 
     return matrix
+
+def remainingEdges(matrix, path):
+    """ Given a path and adj matrix it returns a list of nodes not visited in the path
+    """
+    res = []
+    for i, j in enumerate(matrix[path[-1]]):
+        if i not in path:
+            res.append(j)
+    # print(res)
+    return res if res != [] else [0]
+
+def remainingNodes(n, path):
+    # return list(set((i for i in range(n)))-set(path))
+    res = []
+    for i in range(n):
+        if i not in path:
+            res.append(i)
+    return res
+
+# Sourced from Geeks for Geeks 
+# Python implementation for Kruskal's Algorithm
+# Finds MST using Kruskal's algorithm  
+def kruskalMST(cost): 
+    V = len(cost)
+    parent = [i for i in range(V)] 
+    mincost = 0 # Cost of min MST 
+    
+    def find(i): 
+        while parent[i] != i: 
+            i = parent[i] 
+        return i   
+
+    def union(i, j): 
+        a = find(i) 
+        b = find(j) 
+        parent[a] = b 
+
+    # Initialize sets of disjoint sets 
+    for i in range(V): 
+        parent[i] = i 
+  
+    # Include minimum weight edges one by one  
+    edge_count = 0
+    edge_list = []
+    while edge_count < V - 1: 
+        min = float('inf') 
+        a = -1
+        b = -1
+        for i in range(V): 
+            for j in range(V): 
+                if find(i) != find(j) and cost[i][j] < min: 
+                    min = cost[i][j] 
+                    a = i 
+                    b = j 
+        union(a, b) 
+        edge_list.append((a,b))
+        # print('Edge {}:({}, {}) cost:{}'.format(edge_count, a, b, min)) 
+        edge_count += 1
+        mincost += min
+  
+    # print("Minimum cost= {}".format(mincost)) 
+    # return (edge_list, mincost)
+    return mincost
